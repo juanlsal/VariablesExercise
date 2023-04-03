7a2561b7f84bdf878f9f16cf1805b55a9db4a1e4
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-02 23:01:45 -0500

    completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Juan";
+            int age = 22;
+            char grade = 'A';
+            bool aGoat = true;
+            double weight = 174.30d;
+            decimal chanceOfFailing = 0.0002m;
+
+            Console.WriteLine($"my name is {name} i am {age} years old. I am now at truecoders and I have an {grade} in my class. I would say my goat status is {aGoat}. Also I am {weight} pounds and have a {chanceOfFailing}% chance of failing");
         }
     }
 }
